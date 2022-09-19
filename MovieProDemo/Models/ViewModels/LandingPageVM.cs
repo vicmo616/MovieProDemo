426e8224a7cc7f3e63cb2ae382fb98cc179eb348
Victor M. Ortiz <vicortiz1993@gmail.com>
2022-09-19 00:55:28 -0400

    Create LandingPageVM.cs
    Rounding out my set of Models by Building a View Model that will be used on the application landing page.

---
+++ MovieProDemo/Models/ViewModels/LandingPageVM.cs
@@ -0,0 +1,19 @@
+﻿using MovieProDemo.Models.Database;
+using MovieProDemo.Models.TMDB;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieProDemo.Models.ViewModels
+{
+    public class LandingPageVM
+    {
+        public List<Collection> CustomCollections { get; set; }
+        public MovieSearch NowPlaying { get; set; }
+        public MovieSearch Popular { get; set; }
+        public MovieSearch TopRated { get; set; }
+        public MovieSearch Upcoming { get; set; }
+    }
+}
