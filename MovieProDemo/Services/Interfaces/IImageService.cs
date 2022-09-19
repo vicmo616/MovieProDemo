003c8b067079c42d58a86c2e550b4d6707715f21
Victor M. Ortiz <vicortiz1993@gmail.com>
2022-09-19 15:05:36 -0400

    Create IImageService.cs
    Created an IImageService Interface which defines the minimum set of functionality when working with images in our application. The details of the service will be concerned with tranforming images into a data format suitable for storage into a database. as a consequnce with displaying these images we will have to convert them again into a format suitable for display.

---
+++ MovieProDemo/Services/Interfaces/IImageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieProDemo.Services.Interfaces
+{
+    public interface IImageService
+    {
+        Task<byte[]> EncodeImageAsync(IFormFile poster);
+
+        Task<byte[]> EncodeImageURLAsync(string imageURL);
+
+        string DecodeImage(byte[] poster, string contentType);
+    }
+}
