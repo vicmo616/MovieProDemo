e6a666b562155d61e2cd23ebea88b124dbfef38a
Victor M. Ortiz <vicortiz1993@gmail.com>
2022-09-20 23:18:29 -0400

    Update HomeController.cs
    worked on the controller home index action which provides a medium for the user to make requests.

--- MovieProDemo/Controllers/HomeController.cs
+++ MovieProDemo/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieProDemo.Data;
+using MovieProDemo.Enums;
 using MovieProDemo.Models;
+using MovieProDemo.Models.ViewModels;
+using MovieProDemo.Services.Interfaces;
 using System.Diagnostics;
 
 namespace MovieProDemo.Controllers
@@ -7,15 +12,33 @@ namespace MovieProDemo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IRemoteMovieService _tmdbMovieService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IRemoteMovieService tmdbMovieService)
         {
             _logger = logger;
+            _context = context;
+            _tmdbMovieService = tmdbMovieService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            const int count = 16;
+            var data = new LandingPageVM()
+            {
+                CustomCollections = await _context.Collection
+                                            .Include(c => c.MovieCollections)
+                                            .ThenInclude(mc => mc.Movie)
+                                            .ToListAsync(),
+                NowPlaying = await _tmdbMovieService.SearchMovieDetailAsync(MovieCategory.now_playing, count),
+                Popular = await _tmdbMovieService.SearchMovieDetailAsync(MovieCategory.popular, count),
+                TopRated = await _tmdbMovieService.SearchMovieDetailAsync(MovieCategory.top_rated, count),
+                Upcoming = await _tmdbMovieService.SearchMovieDetailAsync(MovieCategory.upcoming, count)
+
+            };
+            return View(data);
+           
         }
 
         public IActionResult Privacy()
