b86d1dd2fd51861dfda36bd33af8731b1be8da07
Victor M. Ortiz <vicortiz1993@gmail.com>
2022-09-22 16:04:45 -0400

    Created ActorsController.cs
    Built out the actors controller, this will only have a single action named detail that takes in an argument of type int, representing the unique cast id assigned by the TMDB API. the purpose of this is to allow the user to request actor detail and in order to do that, we'll need access to both the IRemoteMovieService as well as the IMappingService via dependency injection.

---
+++ MovieProDemo/Controllers/ActorsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MovieProDemo.Data;
+using MovieProDemo.Models.Database;
+using MovieProDemo.Models.Settings;
+using MovieProDemo.Services.Interfaces;
+
+namespace MovieProDemo.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _mappingService;
+
+        public ActorsController(IRemoteMovieService tmdbMovieService, IDataMappingService mappingService)
+        {
+            _tmdbMovieService = tmdbMovieService;
+            _mappingService = mappingService;
+        }
+
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var actor = await _tmdbMovieService.ActorDetailAsync(id);
+            actor = _mappingService.MapActorDetail(actor);
+
+            return View(actor); 
+        }
+    }
+}
